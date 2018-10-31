5bcaecad0ba562722a11cbaa9af156c7dbf1269f
oldtrafford <steve.emmanuelck@gmail.com>
2018-10-31 20:00:13 +0530

    removed useless code from last commit

--- app.py
+++ app.py
@@ -7,10 +7,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lotteries.db'
 app.secret_key = "thisisthepasswordforkeralastate"
 
 db = SQLAlchemy(app)
-
-import sys
-path = '/home/steveisredatw/keralastatelotteriesresult/'
-if path not in sys.path:
-   sys.path.insert(0, path)
-
-from app import app as application
