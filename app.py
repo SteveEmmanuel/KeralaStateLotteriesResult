589196ded6e201de81773d9876685625cde80cd2
oldtrafford <steve.emmanuelck@gmail.com>
2018-10-31 19:51:14 +0530

    added code for pythonanywhere

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lotteries.db'
-app.secret_key = "flask rocks!"
+app.secret_key = "thisisthepasswordforkeralastate"
 
 db = SQLAlchemy(app)
+
+import sys
+path = '/home/steveisredatw/keralastatelotteriesresult/'
+if path not in sys.path:
+   sys.path.insert(0, path)
+
+from app import app as application
